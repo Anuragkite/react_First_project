9a70740026725aad8b8b105a145ad310bcc90d28
Anuragkite <Anuragkite143@gmail.com>
2025-10-23 11:42:58 +0530

    not fixed

--- 09ThemeSwitcher/src/App.jsx
+++ 09ThemeSwitcher/src/App.jsx
@@ -24,14 +24,22 @@ function App() {
     setThemeMode("light");
   };
 
+ 
+ const html = document.documentElement;
+html.classList.remove("dark", "light");
+html.classList.add("light");
+
+
   useEffect(() => {
-    document.querySelector("html").classList.remove("dark", "light");
+    document.querySelector("html").classList.remove( "light");
     document.querySelector("html").classList.add(themeMode);
   }, [themeMode]);
 
+
+
   return (
     <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
-      <div className="flex flex-wrap min-h-screen items-center">
+      <div className="flex flex-wrap min-h-screen items-center bg-gray-100 dark:bg-gray-900">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
             <ThemeBtn />
